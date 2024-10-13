aa85c5e1e6fa0f74b42144dc2c583248b2ac8bb3
白書オオキ <041.whitepaper@gmail.com>
2024-10-13 19:02:02 +0900

    batの表示を修正

--- whisper-start.bat
+++ whisper-start.bat
@@ -1,13 +1,13 @@
 @echo off
 REM 仮想環境を有効にする（必要な場合）
 call ./venv/scripts/activate
-
 echo message: venv on
 
+
 REM Python スクリプトを実行する
+echo message: run whisper_transcription.py
 python whisper_transcription.py
 
-echo message: run whisper_transcription.py
 
 REM 実行が完了した後にウィンドウを保持する（任意）
 echo message: done enter to escape
